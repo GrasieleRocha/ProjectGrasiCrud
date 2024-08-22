8dbc63a434cd067893c95168985e971429aad120
Grasiele Miranda <grasiele_rocha@hotmail.com>
2024-08-22 11:34:09 -0300

    estilização do user-list-component

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,12 +1,12 @@
-// src/app/components/user-list/user-list.component.ts
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
-import { Router, RouterModule } from '@angular/router';  // Importa Router e RouterModule
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css'],
   standalone: true,
   imports: [RouterModule, CommonModule],
 })
@@ -22,7 +22,7 @@ export class UserListComponent implements OnInit {
   }
 
   editUser(user: any): void {
-    this.router.navigate(['/users/edit', user.id]);  // Implementa navegação para edição
+    this.router.navigate(['/users/edit', user.id]); 
   }
 
   deleteUser(userId: number): void {
